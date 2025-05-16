578ae36a2a43840f158e0234f64af476bb91abb9
Rutu2012 <sanghanirutu944@gmail.com>
2025-05-16 11:21:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+    // Function to update content on button click
+    document.getElementById('enterBtn').addEventListener('click', function() {
+      const statusElement = document.getElementById('status');
+      statusElement.innerHTML = '<h1>Entered Metaverse</h1>';
+    });
+  
